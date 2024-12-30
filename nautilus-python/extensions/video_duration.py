c3059e35c30a15a8ef7ef3a9a895c00d8c34c175
Bron <bpardo@outlook.com>
2024-12-30 03:09:16 -0800

    Update video_duration.py
    updated to speed up process for large number of files. referenced here: https://stackoverflow.com/questions/9660065/speed-up-nautilus-python-extensions-for-reading-images-exif

--- nautilus-python/extensions/video_duration.py
+++ nautilus-python/extensions/video_duration.py
@@ -13,21 +13,34 @@ class VideoDurationColumnExtension(GObject.GObject, Nautilus.ColumnProvider, Nau
                                 label="Duration",
                                 description="Shows the duration of video files")]
 
-    def update_file_info(self, file):
+    def update_file_info_full(self, provider, handle, closure, file):
+        duration = ''
         if file.get_mime_type().startswith("video"):
-            file_path = file.get_location().get_path()
-            try:
-                result = subprocess.run(['mediainfo', '--Inform=Video;%Duration%', file_path],
-                                        stdout=subprocess.PIPE,
-                                        stderr=subprocess.PIPE,
-                                        text=True)
-                duration_ms = int(result.stdout.strip())
-                duration = self.convert_millis(duration_ms)
-                file.add_string_attribute('video_duration', duration)
-            except Exception as e:
-                file.add_string_attribute('video_duration', '')
-        else:
-            file.add_string_attribute('video_duration', '')
+            GObject.timeout_add_seconds(1, self.update_duration, provider, handle, closure, file)
+            return Nautilus.OperationResult.IN_PROGRESS
+            
+        file.add_string_attribute('video_duration', duration)
+        
+        return Nautilus.OperationResult.COMPLETE
+            
+    def update_duration(self, provider, handle, closure, file):
+        file_path = file.get_location().get_path()
+        try:
+            result = subprocess.run(['mediainfo', '--Inform=Video;%Duration%', file_path],
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE,
+                                    text=True)
+            duration_ms = int(result.stdout.strip())
+            duration = self.convert_millis(duration_ms)
+        except Exception as e:
+            duration = ''
+            
+        file.add_string_attribute('video_duration', duration)
+        
+        Nautilus.info_provider_update_complete_invoke(closure, provider, 
+                               handle, Nautilus.OperationResult.COMPLETE)
+
+        return False
 
     def convert_millis(self, millis):
         seconds = (millis / 1000) % 60
